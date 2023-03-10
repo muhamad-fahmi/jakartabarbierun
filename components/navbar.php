f98942184b9d007c8e12b4cc23eba5f3c81e92e1
muhamad-fahmi <muhamad.fahmi@outlook.com>
2023-03-10 09:37:15 +0700

    change root dir

--- components/navbar.php
+++ components/navbar.php
@@ -1,6 +1,6 @@
 <section id="navbar">
     <nav class="navbar navbar-expand-lg fixed-top bg-primary">
-        <a class="navbar-brand" href="/jakartabarbierun">
+        <a class="navbar-brand" href="/jbr2023">
             <img src="assets/images/webpage/logo_barbie_white.svg" alt="Jakarta Barbie Run 2023" width="90">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
@@ -11,20 +11,20 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav mx-lg-auto">
                 <li class="nav-item active">
-                    <a class="nav-link text-light px-lg-4" href="/jakartabarbierun/#">Home</a>
+                    <a class="nav-link text-light px-lg-4" href="/jbr2023/#">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-light px-lg-4" href="/jakartabarbierun/#sponsor">Sponsor & Partners</a>
+                    <a class="nav-link text-light px-lg-4" href="/jbr2023/#sponsor">Sponsor & Partners</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-light px-lg-4" href="/jakartabarbierun/#race_information">Race
+                    <a class="nav-link text-light px-lg-4" href="/jbr2023/#race_information">Race
                         Information</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-light px-lg-4" href="/jakartabarbierun/#about_event">About Event</a>
+                    <a class="nav-link text-light px-lg-4" href="/jbr2023/#about_event">About Event</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-light px-lg-4" href="/jakartabarbierun/#contact">Contact Us</a>
+                    <a class="nav-link text-light px-lg-4" href="/jbr2023/#contact">Contact Us</a>
                 </li>
             </ul>
 
